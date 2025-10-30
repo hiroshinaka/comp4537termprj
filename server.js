e4afa53523a2d1e5f5b4bde129bf066d916ac766
Hiroshi <mnakasone3@my.bcit.ca>
2025-10-30 00:02:53 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,284 @@
+require('./utils');
+
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+
+const database = include('sqlConnection');
+const db_utils = include('database/db_utils');
+const db_users = include('database/users');
+const success = db_utils.printMySQLVersion();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const expireTime =  1 * 60 * 60 * 1000 ; //expires after 1 hour  (hours * minutes * seconds * millis)
+
+
+/* secret information section */
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: false}));
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@hiroshi.7b9ukpb.mongodb.net/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore, //default is memory store 
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
+app.get('/', (req,res) => {
+    res.render("index");
+});
+
+app.get('/about', (req,res) => {
+    var color = req.query.color;
+    if (!color) {
+        color = "black";
+    }
+
+    res.render("about", {color: color} );
+});
+
+app.get('/contact', (req,res) => {
+    var missingEmail = req.query.missing;
+    res.render("contact", {missing: missingEmail});
+});
+
+app.post('/submitEmail', (req,res) => {
+    var email = req.body.email;
+    if (!email) {
+        res.redirect('/contact?missing=1');
+    }
+    else {
+        res.render("submitEmail", {email: email});
+    }
+});
+
+app.get('/createTables', async (req,res) => {
+
+    const create_tables = include('database/create_tables');
+
+    var success = create_tables.createTables();
+    if (success) {
+        res.render("successMessage", {message: "Created tables."} );
+    }
+    else {
+        res.render("errorMessage", {error: "Failed to create tables."} );
+    }
+});
+
+app.get('/createUser', (req,res) => {
+    res.render("createUser");
+});
+
+
+app.get('/login', (req,res) => {
+    res.render("login");
+});
+
+app.post('/submitUser', async (req,res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+    var success = await db_users.createUser({ user: username, hashedPassword: hashedPassword });
+
+    if (success) {
+        var results = await db_users.getUsers();
+
+        res.render("submitUser",{users:results});
+    }
+    else {
+        res.render("errorMessage", {error: "Failed to create user."} );
+    }
+
+});
+app.post('/signout', (req, res) => {
+    // Destroy the session to log the user out
+    req.session.destroy((err) => {
+        if (err) {
+            console.error("Error destroying session:", err);
+            res.status(500).send("Could not sign out. Please try again.");
+        } else {
+            // Redirect to the home page or login page after signing out
+            res.redirect('/');
+        }
+    });
+});
+
+
+app.post('/loggingin', async (req,res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+
+    var results = await db_users.getUser({ user: username, hashedPassword: password });
+
+    if (results) {
+        if (results.length == 1) { //there should only be 1 user in the db that matches
+            if (bcrypt.compareSync(password, results[0].password)) {
+                req.session.authenticated = true;
+                req.session.user_type = results[0].type; 
+                req.session.username = username;
+                req.session.cookie.maxAge = expireTime;
+        
+                res.redirect('/loggedIn');
+                return;
+            }
+            else {
+                console.log("invalid password");
+            }
+        }
+        else {
+            console.log('invalid number of users matched: '+results.length+" (expected 1).");
+            res.redirect('/');
+            return;            
+        }
+    }
+
+    console.log('user not found');
+    //user and password combination not found
+    res.redirect("/");
+});
+
+
+
+
+function isValidSession(req) {
+	if (req.session.authenticated) {
+		return true;
+	}
+	return false;
+}
+
+function sessionValidation(req, res, next) {
+	if (!isValidSession(req)) {
+		req.session.destroy();
+		res.redirect('/');
+		return;
+	}
+	else {
+		next();
+	}
+}
+
+function isAdmin(req) {
+    if (req.session.user_type == 'admin') {
+        return true;
+    }
+    return false;
+}
+
+function adminAuthorization(req, res, next) {
+	if (!isAdmin(req)) {
+        res.status(403);
+        res.render("errorMessage", {error: "Not Authorized"});
+        return;
+	}
+	else {
+		next();
+	}
+}
+
+app.use('/loggedin', sessionValidation);
+app.use('/loggedin/admin', adminAuthorization);
+
+app.get('/loggedin', (req,res) => {
+    res.render("loggedin");
+});
+
+app.get('/loggedin/info', (req,res) => {
+    res.render("loggedin-info");
+});
+
+app.get('/loggedin/admin', (req,res) => {
+    res.render("admin");
+});
+
+app.get('/loggedin/memberinfo', (req,res) => {
+    res.render("memberInfo", {username: req.session.username, user_type: req.session.user_type});
+});
+
+
+app.get('/rilla/:id', (req,res) => {
+    var rilla = req.params.id;
+
+    res.render("rilla", {rilla: rilla});
+});
+
+
+app.get('/api', (req,res) => {
+	var user = req.session.user;
+    var user_type = req.session.user_type;
+	console.log("api hit ");
+
+	var jsonResponse = {
+		success: false,
+		data: null,
+		date: new Date()
+	};
+
+	
+	if (!isValidSession(req)) {
+		jsonResponse.success = false;
+		res.status(401);  //401 == bad user
+		res.json(jsonResponse);
+		return;
+	}
+
+	if (typeof id === 'undefined') {
+		jsonResponse.success = true;
+		if (user_type === "admin") {
+			jsonResponse.data = ["A","B","C","D"];
+		}
+		else {
+			jsonResponse.data = ["A","B"];
+		}
+	}
+	else {
+		if (!isAdmin(req)) {
+			jsonResponse.success = false;
+			res.status(403);  //403 == good user, but, user should not have access
+			res.json(jsonResponse);
+			return;
+		}
+		jsonResponse.success = true;
+		jsonResponse.data = [id + " - details"];
+	}
+
+	res.json(jsonResponse);
+
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req,res) => {
+	res.status(404);
+	res.render("404");
+})
+
+app.listen(port, () => {
+	console.log("Node application listening on port "+port);
+}); 
